5201027e577241cc38b077ea1986bfbade7b643b
YimKeul <leesjun29@gmail.com>
2023-02-24 15:01:24 +0900

    Initial Commit

---
+++ RecommandMenu/RecommandMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecommandMenuApp.swift
+//  RecommandMenu
+//
+//  Created by yimkeul on 2023/02/24.
+//
+
+import SwiftUI
+
+@main
+struct RecommandMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
